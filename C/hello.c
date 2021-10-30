dd2121f2519aae519bc3cce2c4765a75deb2ed7b
Guan <guan.yang@hornblower.com>
2021-10-29 18:21:33 -0700

    remove unused

--- C/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
- 
-int main()
-{
-    /* Hello */
-    printf("Hello, World! \n");
- 
-    return 0;
-}
--- Swift/hello.swift
+++
@@ -1,7 +0,0 @@
-import Cocoa
-
-/* My First Swift App */
-
-var myString = "Hello, World!"
-
-print(myString)
